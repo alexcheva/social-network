917b2ccec5ea0cf4be03da69b758106b3ddd2add
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-05-26 19:26:06 -0700

    Change scrolling script

--- index.php
+++ index.php
@@ -48,51 +48,64 @@ if(isset($_POST['post'])){
 	</div>
 
 	<script>
-		var userLoggedIn = '<?php echo $userLoggedIn; ?>'
-
-		$(document).ready(function(){
+	$(function(){
+	 
+		var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+		var inProgress = false;
+	 
+		loadPosts(); //Load first posts
+	 
+	    $(window).scroll(function() {
+	    	var bottomElement = $(".status_post").last();
+	    	var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+	 
+	        // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
+	        if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+	            loadPosts();
+	        }
+	    });
+	 
+	    function loadPosts() {
+	        if(inProgress) { //If it is already in the process of loading some posts, just return
+				return;
+			}
+			
+			inProgress = true;
 			$('#loading').show();
-			//original ajax request for loading first posts
+	 
+			var page = $('.posts_area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
+	 
 			$.ajax({
 				url: "includes/handlers/ajax_load_posts.php",
 				type: "POST",
-				data: "page=1&userLoggedIn=" + userLoggedIn,
+				data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
 				cache: false,
-
-				success: function(data) {
-					$('#loading').hide();//when posts returned, don't show icon anymore
-					$('.posts_area').html(data);//put data into the div
+	 
+				success: function(response) {
+					$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+					$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+					$('.posts_area').find('.noMorePostsText').remove(); //Removes current .nextpage 
+	 
+					$('#loading').hide();
+					$(".posts_area").append(response);
+	 
+					inProgress = false;
 				}
 			});
-
-			$(window).scroll(function(){
-				var height = $('.posts_area').height(); // height of div containing posts
-				var scroll_top = $(this).scrollTop();
-				var page = $('.posts_area').find('.nextPage').val();//find the value of nextPage div
-				var noMorePosts = $('.posts_area').find('.noMorePosts').val();//find value of noMorePosts div
-
-				if((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
-					$('#loading').show();//show loading icon
-
-					var ajaxReq = $.ajax({
-						url: "includes/handlers/ajax_load_posts.php",
-						type: "POST",
-						data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-						cache: false,
-
-						success: function(response) {
-							$('.posts_area').find('.nextPage').remove();//removes current nextPage div
-							$('.posts_area').find('.noMorePosts').remove();
-
-							$('#loading').hide();
-							$('.posts_area').append(response);
-						}
-					});
-				}//end if statement
-				return false;
-			});//end (window).scroll(function())
-
-		});//end document.ready
+	    }
+	 
+	    //Check if the element is in view
+	    function isElementInView (el) {
+	        var rect = el.getBoundingClientRect();
+	 
+	        return (
+	            rect.top >= 0 &&
+	            rect.left >= 0 &&
+	            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
+	            rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+	        );
+	    }
+	});
 	</script>
 
 	</div>
