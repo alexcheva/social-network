fcc34e0135a955fba5321b7dda5a02b2c0b4c5e4
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-05-28 20:30:33 -0700

    update infine scroll to profile

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-include('includes/header.php');
+	include('includes/header.php');
 
-//handle posting of posts
-//redirect back to index
-if(isset($_POST['post'])){
-	$post = new Post($con, $userLoggedIn);
-	$post->submitPost($_POST['post_text'], 'none');
-	// header("Location: index.php");
-}
+	//handle posting of posts
+	//redirect back to index
+	if(isset($_POST['post'])){
+		$post = new Post($con, $userLoggedIn);
+		$post->submitPost($_POST['post_text'], 'none');
+		// header("Location: index.php");
+	}
 
 ?>
 	<div class="user_details column">
@@ -18,32 +18,26 @@ if(isset($_POST['post'])){
 				echo $user['first_name'] . " " . $user['last_name'];
 				?>
 			</a>
-			<p>			<?php 
+			<p>
+				<?php 
 				echo "Posts: " . $user['num_posts'] . "<br>";
 				echo "Likes: " . $user['num_likes'];
-
-			?></p>
-			
-
-		</div>
-		
-
-
-
+				?>
+			</p>
+		</div>		
 	</div>
 	<div class="main_column column">
-	<form class="post_form" action="index.php" method="POST">
-		<textarea name="post_text" id="post_text"  placeholder="Write a post"></textarea>
-<!-- 		<br>
-		<input type="radio" id="global" name="visibility" value="global" checked="checked">
-		<i class="fas fa-globe radio"></i>
-		<input type="radio" id="friends_only" name="visibility" value="friends_only">
-		<i class="fas fa-user-friends radio"></i>
- -->		<input type="submit" name="post" id="post_button" value="Post">
-	</form>
-	<hr>
-
-	<div class="posts_area"></div>
+		<form class="post_form" action="index.php" method="POST">
+			<textarea name="post_text" id="post_text"  placeholder="Write a post"></textarea>
+	<!-- 		<br>
+			<input type="radio" id="global" name="visibility" value="global" checked="checked">
+			<i class="fas fa-globe radio"></i>
+			<input type="radio" id="friends_only" name="visibility" value="friends_only">
+			<i class="fas fa-user-friends radio"></i>
+	 -->	<input type="submit" name="post" id="post_button" value="Post">
+		</form>
+		<hr>
+		<div class="posts_area"></div>
 		<img id="loading" src="assets/images/icons/loading.gif">
 	</div>
 
@@ -107,7 +101,7 @@ if(isset($_POST['post'])){
 	    }
 	});
 	</script>
-
+	<!-- end wrapper from header -->
 	</div>
 	</body>
 </html>
