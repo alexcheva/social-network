9bc50c8993d8fb9702d1c129dd4e98015ff3c9b3
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-06-06 18:24:09 -0700

    added local bootstrap css

---
+++ includes/handlers/ajax_load_messages.php
@@ -0,0 +1,10 @@
+<?php 
+include("../../config/config.php");
+include("../classes/User.php");
+//for messages:
+//$limit = 7;
+//$message = new Message ($con, $_REQUEST['userLoggedIn']);
+// echo $message->GetConvosDropdown($_REQUEST, $limit);
+
+
+ ?>
