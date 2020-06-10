8d5c82c1f1893fa46cf6d6cf5feaa3fdae3f4626
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-06-09 17:26:05 -0700

    add ajax_load_notifications handler

---
+++ includes/handlers/ajax_load_notifications.php
@@ -0,0 +1,12 @@
+<?php 
+include("../../config/config.php");
+include("../classes/User.php");
+include("../classes/Notification.php");
+
+
+$limit = 7;
+$notification = new Notification ($con, $_REQUEST['userLoggedIn']);
+echo $notification->getNotifications($_REQUEST, $limit);
+
+
+ ?>
