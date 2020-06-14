08e0b2769acc07530e83dda86ff724dece30d523
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-06-13 20:43:27 -0700

    wordking on settings

--- includes/form_handlers/settings_handler.php
+++ includes/form_handlers/settings_handler.php
@@ -1,15 +1,26 @@
 <?php 
 
-if(isset($_POST['update_detais'])){
+if(isset($_POST['update_details'])){
+	// $id = $_POST['id'];
 	$first_name = $_POST['first_name'];
 	$last_name = $_POST['last_name'];
-	$username = $_post['username'];
+	// $username = $_POST['username'];
 	$email = $_POST['email'];
-
+	//check if the email is in use:
 	$email_check = mysqli_query($con, "SELECT * FROM users WHERE email='$email'");
 	$row = mysqli_fetch_array($email_check);
+	$matched_user = $row['username'];
 
+	if($matched_user == "" || $matched_user == $userLoggedIn){
+		$message = "<p>Details updated!</p>";
 
-}
+		$query = mysqli_query($con, "UPDATE users SET first_name='$first_name', last_name='$last_name', email='$email' WHERE username='$userLoggedIn'");
+
+	}else
+	$message = "<p>That email is already in use!</p>";
+
+} else
+$message = "";
+
 
 ?>
--- settings.php
+++ settings.php
@@ -1,38 +1,38 @@
 <?php 
 include("includes/header.php");
-// include("includes/settings_handler.php");
+include("includes/form_handlers/settings_handler.php");
 
  ?>
 
  <div class="main_column column">
  	
- 	<h4>Account Settings</h4>
+ 	<h4>Account Settings:</h4>
  	<?php 
  	echo "<img src='" . $user['profile_pic'] . "' id='small_profile_pic'>";
  	 ?>
  	 <div>
  	 	<a href="upload.php">Upload new profile picture</a>
  	 	<p>Modify the values and click "Update Details":</p>
- 	 	<form class="settings" id="settings" action="settings.php" method="POST">
- 	 		<label>First Name:</label>
+ 	 	<form class="settings" action="settings.php" method="POST">
+<!--  	 		<input type="hidden" name="id" value="<?php echo $user['id']; ?>">			 --> 	 		<label>First Name:</label>
  	 		<input type="text" name="first_name" value="<?php echo $user['first_name']; ?>">
  	 		<label>Last Name:</label>
  	 		<input type="text" name="last_name" value="<?php echo $user['last_name']; ?>">
- 	 		<label>Username:</label>
- 	 		<input type="text" name="username" value="<?php echo $user['username']; ?>">
+ 	 		<!-- <label>Username:</label>
+ 	 		<input type="text" name="username" value="<?php echo $user['username']; ?>"> -->
  	 		<label>Email:</label>
  	 		<input type="text" name="email" value="<?php echo $user['email']; ?>">
- 	 		<input type="submit" name="update_details" id="save_details" value="Update Details">
+ 	 		<input type="submit" name="update_details" class="save_details" value="Update Details">
  	 	</form>
  	 	<h4>Change Password:</h4>
- 	 	<form class="settings" id="settings" action="settings.php" method="POST">
+ 	 	<form class="settings" action="settings.php" method="POST">
  	 		<label>Old Password:</label>
  	 		<input type="password" name="old_password">
  	 		<label>New Password:</label>
  	 		<input type="password" name="new_password1">
  	 		<label>New Password Again:</label>
  	 		<input type="password" name="new_password2">
- 	 		<input type="submit" name="update_password" id="save_details" value="Update Password">
+ 	 		<input type="submit" name="update_password" class="save_details" value="Update Password">
  	 	</form>
  	 	<h4>Close Account:</h4>
  	 	<form action="close_account.php">
