84a7a83dbe58c443e8cca3d71698d53e255dd3ca
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-06-24 22:46:31 -0700

    delete no posts to show yet

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -140,7 +140,6 @@ class Post{
 		$str = ""; //string to return
 		$data_query = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
 		//if there are posts:
-
 		if(mysqli_num_rows($data_query) > 0){
 
 			$num_iterations = 0; //Number of results checked (not nessasery posted)
@@ -354,12 +353,14 @@ class Post{
 				<?php
 			}//end while loop
 
-			if($count > $limit)
+			//if there are no posts:
+			if(mysqli_num_rows($data_query) == 0){
+				$str = "<p>There are no posts to show yet! Try adding firends or posting something!</p>";
+			}else if($count > $limit)
 				$str .="<input type='hidden' class='nextPage' value='" . ($page + 1) ."'><input type='hidden' class='noMorePosts' value='false'>";//append to str
 				else
 				$str .="<input type='hidden' class='noMorePosts' value='true'><p class='no_posts_p'> No More Posts to show!</p>";
 		}//end if statement
-		$str = "<input type='hidden' class='noMorePosts' value='true'><p class='no_posts_p'> No Posts to show yet! Try adding friends or post something.</p>";
 		echo $str;	
 
 	}
@@ -557,10 +558,8 @@ class Post{
 				$str .="<input type='hidden' class='nextPage' value='" . ($page + 1) ."'><input type='hidden' class='noMorePosts' value='false'>";//append to str
 				else
 				$str .="<input type='hidden' class='noMorePosts' value='true'><p class='no_posts_p'> No More Posts to show!</p>";
-		}else{
-
-		$str = "<input type='hidden' class='noMorePosts' value='true'><p class='no_posts_p'> No Posts to show yet! Try adding friends or post something.</p>";
-		echo $str;	}
+		}//end if statement
+		echo $str;	
 
 	}
 	//send comments
