71baef75be691130b35f4c7a63d323fcbc6667b7
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-07-08 15:14:10 -0700

    pretify index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	include('includes/header.php');
 
-	//handle posting of posts
+	//handle posting
 	//redirect back to index
 	if(isset($_POST['post'])){
 		$uploadOk = 1;
@@ -41,25 +41,24 @@
 			echo "<div style='text-align:center;' class='error'> 
 			$errorMessage</div>";
 		}
-
-
 	}
-
 ?>
 	<div class="user_details column">
-		<a href="<?php echo $userLoggedIn ?>"><img id="profile_pic" src="<?php echo $user['profile_pic']; ?>"></a>
+		<a href="<?php echo $userLoggedIn ?>">
+			<img id="profile_pic" src="<?php echo $user['profile_pic']; ?>">
+		</a>
 		<div class="user_details_left_right">
 			<a href="<?php echo $userLoggedIn ?>" id="name">
 				<?php 
-				echo $user['first_name'] . " " . $user['last_name'];
+					echo $user['first_name'] . " " . $user['last_name'];
 				?>
 			</a>
 			<p>
 				<?php 
-				$get_number_posts = mysqli_query($con, "SELECT * FROM posts WHERE added_by='$userLoggedIn'");
-		$num_posts = mysqli_num_rows($get_number_posts);
-				echo "Posts: " . $num_posts . "<br>";
-				echo "Likes: " . $user['num_likes'];
+					$get_number_posts = mysqli_query($con, "SELECT * FROM posts WHERE added_by='$userLoggedIn'");
+					$num_posts = mysqli_num_rows($get_number_posts);
+					echo "Posts: " . $num_posts . "<br>";
+					echo "Likes: " . $user['num_likes'];
 				?>
 			</p>
 		</div>		
@@ -78,13 +77,12 @@
 			<i class="fas fa-user-friends radio"></i>
 
 	 -->	
-		<!-- <img src='assets/images/icons/emoji.png' class='toggle_emojis' title="Show emojis">
-		<div class="emojis hide"><?php echo Emojis::$emoji_list; ?></div> -->
-		<a href="javaScript:void(0)" onClick="showFileUpload()">
-			<i class="fas fa-file-image" id="toggle_file_upload"></i></a>
+			<a href="javaScript:void(0)" onClick="showFileUpload()">
+				<i class="fas fa-file-image" id="toggle_file_upload"></i>
+			</a>
 			<input type="file" name="fileToUpload" id="fileToUpload" class="hide">
 		 	<input type="submit" name="post" id="post_button" value="Post">
-	
+
 		</form>
 		<hr>
 		<div class="posts_area"></div>
@@ -93,12 +91,12 @@
 
 	<!-- Post on wall Edit post -->
 	<?php 
-	if(isset($_GET['post_id'])){
-	$post_id = $_GET['post_id'];
-	$get_post = mysqli_query($con, "SELECT body FROM posts WHERE post_id='$post_id'");
-	$old_post = $row['body'];
+		if(isset($_GET['post_id'])){
+		$post_id = $_GET['post_id'];
+		$get_post = mysqli_query($con, "SELECT body FROM posts WHERE post_id='$post_id'");
+		$old_post = $row['body'];
 	?>
-<div class="modal fade" id="edit_post<?php echo $post_id;?>" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
+	<div class="modal fade" id="edit_post<?php echo $post_id;?>" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
 	  <div class="modal-dialog" role="document">
 	    <div class="modal-content">
 
@@ -126,129 +124,129 @@
 	  </div>
 	</div>
 	<?php
-	}
+		}
 
-	if(isset($_POST['post_button'])){
-	
-	$new_post = strip_tags($_POST['new_post']);
+		if(isset($_POST['post_button'])){
 		
-	//set post in posts table deleted field to yes
-	$query = mysqli_query($con, "UPDATE posts SET body='$new_post' WHERE id='$post_id'");
-	}
-	 ?>
-	
-
+			$new_post = strip_tags($_POST['new_post']);
+				
+			//set post in posts table deleted field to yes
+			$query = mysqli_query($con, "UPDATE posts SET body='$new_post' WHERE id='$post_id'");
+		}
+	?>
+		
 	<script>
 
-	$(function(){
-	 
-		var inProgress = false;
+		$(function(){
+		 
+			var inProgress = false;
 
-		loadPosts(); //Load first posts
-	    $(window).scroll(function() {
-	    	var bottomElement = $(".status_post").last();
-	    	var noMorePosts = $('.posts_area').find('.noMorePosts').val();
-	 
-	        // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
-	        if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
-	            loadPosts();
-	        }
-	    });
-	 
-	    function loadPosts() {
-	        if(inProgress) { //If it is already in the process of loading some posts, just return
-				return;
-			}
-			
-			inProgress = true;
-			$('#loading').show();
-	 
-			var page = $('.posts_area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
-	 
-			$.ajax({
-				url: "includes/handlers/ajax_load_posts.php",
-				type: "POST",
-				data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-				cache: false,
-	 
-				success: function(response) {
-					$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
-					$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
-					$('.posts_area').find('.noMorePostsText').remove(); //Removes current .nextpage 
-	 
-					$('#loading').hide();
-					$(".posts_area").append(response);
-					inProgress = false;
+			loadPosts(); //Load first posts
+		    $(window).scroll(function() {
+		    	var bottomElement = $(".status_post").last();
+		    	var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+		 
+		        // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
+		        if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+		            loadPosts();
+		        }
+		    });
+		 
+		    function loadPosts() {
+		        if(inProgress) { //If it is already in the process of loading some posts, just return
+					return;
 				}
-			});
-	    }
-	 
-	    //Check if the element is in view
-	    function isElementInView (el) {
-	        var rect = el.getBoundingClientRect();
-	 
-	        return (
-	            rect.top >= 0 &&
-	            rect.left >= 0 &&
-	            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
-	            rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
-	        );
-	    }
-	});
-	</script>
-	<script>function sendComment(id) {
- 	const userLoggedIn = '<?php echo $userLoggedIn; ?>';
-	const commentText = $("#comment" + id).val();
-	
-	if(commentText === "") {
- 
-		bootbox.alert("Please enter some text first!");
-		return;
-	}
- 
-	const sendComment = $.post("includes/handlers/send_comment.php", {
-			userLoggedIn: userLoggedIn, 
-			commentText: commentText, 
-			id: id
-		}, 
-		function(response){
- 
-		if(response !== "No text") {
- 
-			const loadComment = $.post("includes/handlers/load_comment.php", 
-				{
-					id: id, 
-					userLoggedIn: userLoggedIn
-				}, 
-				function(newComment) {
- 
-				$("#comment" + id).val("");
-				const noComment = $("#toggleComment" + id).find("#noComment" + id);
 				
-				if(noComment.length !== 0) {
-					noComment.remove();
-				}
- 
-				$("#toggleComment" + id).append(newComment);
- 
-			});
+				inProgress = true;
+				$('#loading').show();
+		 
+				var page = $('.posts_area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
+		 
+				$.ajax({
+					url: "includes/handlers/ajax_load_posts.php",
+					type: "POST",
+					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+					cache: false,
+		 
+					success: function(response) {
+						$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+						$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+						$('.posts_area').find('.noMorePostsText').remove(); //Removes current .nextpage 
+		 
+						$('#loading').hide();
+						$(".posts_area").append(response);
+						inProgress = false;
+					}
+				});
+		    }
+		 
+		    //Check if the element is in view
+		    function isElementInView (el) {
+		        var rect = el.getBoundingClientRect();
+		 
+		        return (
+		            rect.top >= 0 &&
+		            rect.left >= 0 &&
+		            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
+		            rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+		        );
+		    }
+		});
+	</script>
+	<script>
+		function sendComment(id) {
+	 	const userLoggedIn = '<?php echo $userLoggedIn; ?>';
+		const commentText = $("#comment" + id).val();
+		
+		if(commentText === "") {
+	 
+			bootbox.alert("Please enter some text first!");
+			return;
 		}
- 
-		else {
- 
-			bootbox.alert("Something went wrong. Please try again.");
-		} 
- 
-	});
-};
-</script>
-<script>
-$(document).ready(function(){
-	$(".comment").emojioneArea({
-		pickerPosition: "bottom"
-	});
-});
-</script>
+	 
+		const sendComment = $.post("includes/handlers/send_comment.php", {
+				userLoggedIn: userLoggedIn, 
+				commentText: commentText, 
+				id: id
+			}, 
+			function(response){
+	 
+			if(response !== "No text") {
+	 
+				const loadComment = $.post("includes/handlers/load_comment.php", 
+					{
+						id: id, 
+						userLoggedIn: userLoggedIn
+					}, 
+					function(newComment) {
+	 
+					$("#comment" + id).val("");
+					const noComment = $("#toggleComment" + id).find("#noComment" + id);
+					
+					if(noComment.length !== 0) {
+						noComment.remove();
+					}
+	 
+					$("#toggleComment" + id).append(newComment);
+	 
+				});
+			}
+	 
+			else {
+	 
+				bootbox.alert("Something went wrong. Please try again.");
+			} 
+	 
+		});
+	};
+	</script>
+	<script>
+		$(document).ready(function(){
+			$(".comment").emojioneArea({
+				pickerPosition: "bottom"
+			});
+		});
+	</script>
 
 	<!-- end wrapper from header -->
 	</div>
