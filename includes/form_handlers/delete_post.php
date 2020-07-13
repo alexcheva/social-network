458da4f7b06b02d67cc4c4db9a9e9ff113aa5786
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-07-12 18:06:52 -0700

    try update num_post after deleting

--- includes/form_handlers/delete_post.php
+++ includes/form_handlers/delete_post.php
@@ -1,5 +1,6 @@
 <?php 
 require '../../config/config.php';
+// $username = $_POST['userLoggedIn'];
 
 if(isset($_GET['post_id']))
 	$post_id = $_GET['post_id'];
@@ -8,6 +9,10 @@ if(isset($_POST['result'])) {
 	if($_POST['result'] == 'true')
 
 		$query = mysqli_query($con, "DELETE FROM posts WHERE id='$post_id'");
+
+		// $get_number_posts = mysqli_query($con, "SELECT * FROM posts WHERE added_by='$username'");
+		// $num_posts = mysqli_num_rows($get_number_posts);
+		// $query = mysqli_query($con, "UPDATE users SET num_posts='$num_posts' WHERE username='$username'");
 		
 }
 
