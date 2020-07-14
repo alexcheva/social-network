0033afabb0281ec60d9f768e7a2de602a70cc6a3
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-07-13 23:18:39 -0700

    unlink image on delete

--- includes/form_handlers/delete_post.php
+++ includes/form_handlers/delete_post.php
@@ -6,14 +6,20 @@ if(isset($_GET['post_id']))
 	$post_id = $_GET['post_id'];
 
 if(isset($_POST['result'])) {
-	if($_POST['result'] == 'true')
+		$query_image = mysqli_query($con, "SELECT image FROM posts WHERE id='$post_id'");
+		$row = mysqli_fetch_array($query_image);
+		$post_image_src = $row['image'];
+		$image_src = "../../" .$post_image_src;
+		unlink($image_src);
 
-		$query = mysqli_query($con, "DELETE FROM posts WHERE id='$post_id'");
+	if($_POST['result'] == 'true'){
+
+		$delete_post = mysqli_query($con, "DELETE FROM posts WHERE id='$post_id'");
 
 		// $get_number_posts = mysqli_query($con, "SELECT * FROM posts WHERE added_by='$username'");
 		// $num_posts = mysqli_num_rows($get_number_posts);
 		// $query = mysqli_query($con, "UPDATE users SET num_posts='$num_posts' WHERE username='$username'");
-		
+	}
 }
 
  ?>
