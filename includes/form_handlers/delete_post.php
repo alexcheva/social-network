5b6b89f28edc4619b2a3d6d44e516db5581e7bfe
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-07-19 20:39:39 -0700

    delete comments with post

--- includes/form_handlers/delete_post.php
+++ includes/form_handlers/delete_post.php
@@ -6,15 +6,17 @@ if(isset($_GET['post_id']))
 	$post_id = $_GET['post_id'];
 
 if(isset($_POST['result'])) {
+		
+
+	if($_POST['result'] == 'true'){
 		$query_image = mysqli_query($con, "SELECT image FROM posts WHERE id='$post_id'");
 		$row = mysqli_fetch_array($query_image);
 		$post_image_src = $row['image'];
 		$image_src = "../../" .$post_image_src;
 		unlink($image_src);
 
-	if($_POST['result'] == 'true'){
-
 		$delete_post = mysqli_query($con, "DELETE FROM posts WHERE id='$post_id'");
+		$delete_post_comments =  mysqli_query($con, "DELETE FROM comments WHERE post_id='$post_id'");
 
 		// $get_number_posts = mysqli_query($con, "SELECT * FROM posts WHERE added_by='$username'");
 		// $num_posts = mysqli_num_rows($get_number_posts);
