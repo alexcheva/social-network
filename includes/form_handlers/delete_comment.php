36412873d96aee0d679f80f367809af0fa759f83
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-07-19 18:39:07 -0700

    update delete_comment

--- includes/form_handlers/delete_comment.php
+++ includes/form_handlers/delete_comment.php
@@ -6,10 +6,7 @@ if(isset($_GET['comment_id']))
 
 if(isset($_POST['result'])) {
 	if($_POST['result'] == 'true')
-		//set post in posts table deleted field to yes
-		// $query = mysqli_query($con, "UPDATE posts SET deleted='yes' WHERE id='$post_id'");
 		
-		//actually delete post from the table
 		$query = mysqli_query($con, "DELETE FROM comments WHERE id='$comment_id'");
 }
 
