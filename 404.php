f7866f6b8a2d75753e4ea571779cf41bea0fb53b
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-07-22 19:59:05 -0700

    edit 404

--- 404.php
+++ 404.php
@@ -2,10 +2,10 @@
 	include("includes/header.php");
 ?>
 <div class="main_column column" id="main_colum">
-	<h4>404: Page not found.</h4>
-	<img style='width: 175px; margin-bottom: 10px;' src='assets/images/icons/why.png'>
+	<h2>404: Page not found.</h2>
+	<img class='why' src='assets/images/icons/why.png'>
 	
-	<?php echo "<p>".$_SERVER['REQUEST_URI']. " does not exist, sorry.</p>"; ?> 
+	<?php echo "<h4>".$_SERVER['REQUEST_URI']. " does not exist, sorry.</h4>"; ?> 
 	<p>If you clicked a link, it might be broken. You should email admin and let them know there is a broken link on the site.</p>
 	<p>If you got here by typing randomly in the address bar, stop doing that. You're filling my error logs with unnecessary junk. :)</p>
 	<p><a href="index.php">Go back to the main page.</a></p>
