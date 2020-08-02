1da7f4fe7660a106cba4b477b160f46e5e45341f
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2020-08-01 20:37:38 -0700

    added footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+	</div>
+	<footer class="footer_bar bar">
+		<div class="footer-wrapper">
+			2020 Verni Moj 2007.
+			Designed and developed by Alexandra Lukinicheva.
+		</div>
+	</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
+	</body>
+</html>
