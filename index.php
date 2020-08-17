09dd0726ad2f894cc2db2d1d3346a1ae9eee1a0f
Gabriel Hernandez <webspinner.gabriel@gmail.com>
2020-08-16 23:54:53 -0700

    fix bad check for in view element, needs to support element larger than window.
    1 bottle of wine please.

--- index.php
+++ index.php
@@ -156,7 +156,7 @@
 		        var rect = el.getBoundingClientRect();
 		 
 		        return (
-		            rect.top >= 0 &&
+		            rect.top >= (window.innerHeight || document.documentElement.clientHeight) - rect.height &&
 		            rect.left >= 0 &&
 		            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
 		            rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
